4fe898f1bd392094f05da9691331b329174488de
Andrew Morley <morleyandrew84@gmail.com>
2022-12-07 15:26:45 -0600

    updates to counter function

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -7,33 +7,35 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
 using System.Net.Http;
-
+using System.Text;
 
 namespace Company.Function
 {
     public static class GetResumeCounter
     {
         [FunctionName("GetResumeCounter")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzurePersonalWebsite", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzurePersonalWebsite", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1")] out Counter updatedCounter,
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"AzurePersonalWebsite", collectionName: "Counter",
+                ConnectionStringSetting = "AzurePersonalWebsite", Id = "index", PartitionKey = "index")] Counter counter,
+                [CosmosDB(databaseName:"AzurePersonalWebsite", collectionName: "Counter",
+                ConnectionStringSetting = "AzurePersonalWebsite", Id = "index", PartitionKey = "index")] out Counter updatedCounter,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            log.LogInformation("GetResumeCounter was triggered.");
 
             updatedCounter = counter;
             updatedCounter.Count += 1;
 
             var jsonToReturn = JsonConvert.SerializeObject(counter);
 
-            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            return new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
-            }
-
-
+            };
         }
     }
 }
