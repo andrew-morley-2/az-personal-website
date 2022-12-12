80af2eb291b70f1b2ae38eb70a4e0dcb37d07acf
Andrew Morley <morleyandrew84@gmail.com>
2022-12-11 21:01:24 -0600

    Updates to counter, and frontend js

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -1,13 +1,33 @@
-using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Company.Function
 {
-    public class Counter
+    public static class Counter
     {
-        [JsonProperty(PropertyName="id")]
-        public string Id {get; set;}
-        
-        [JsonProperty(PropertyName = "count")]
-        public int Count {get;set;}
+        [Function("Counter")]
+        [CosmosDBOutput("%DataBaseName%", "%CollectionName%", Connection = "CosmosDBConnectionString")]
+        public static object Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [CosmosDBInput("%DatabaseName%", "%CollectionName%", Connection = "CosmosDBConnectionString", Id ="1",PartitionKey ="1")] CounterJson counter,
+            FunctionContext executionContext)
+        {
+            counter.Count++;
+            
+
+            return counter;
+        }
     }
-}        
+}
+
+public class CounterJson
+{
+    [JsonPropertyName("id")]
+    public string Id {get;set;}
+    [JsonPropertyName("count")]
+    public string Count {get;set;}
+}
